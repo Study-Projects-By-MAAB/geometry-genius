16a9c4180b39a41b56800a8d6f228c01a4b3ba55
maab-fw <maabfw@proton.me>
2024-02-13 06:58:24 +0600

    rectangle area completed

--- scripts/rectangle-area.js
+++ scripts/rectangle-area.js
@@ -1,3 +1,15 @@
-function calculateRectangleArea(params) {
-    console.log('rect connected');
+function calculateRectangleArea() {
+    // get length of the rectangle
+    const lengthInput = document.getElementById('rectangle-length')
+    const lengthText = lengthInput.value;
+    const length = parseFloat(lengthText);
+    // console.log(length);
+    // get width of the rectangle
+    const widthInput = document.getElementById('rectangle-width');
+    const widthText = widthInput.value;
+    const width = parseFloat(widthText);
+    // console.log(width);
+    area = length * width
+    const rectangleAreaSpan = document.getElementById('rectangle-area')
+    rectangleAreaSpan.innerText = area
 }
